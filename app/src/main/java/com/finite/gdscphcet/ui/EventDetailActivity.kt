cfc74c692445b0bbfb2a29265009ef9be1a31916
Fin1te <rishabhmehta00@gmail.com>
2022-01-10 01:59:11 +0530

    Firbase Connectivity Successful

--- app/src/main/java/com/finite/gdscphcet/ui/EventDetailActivity.kt
+++ app/src/main/java/com/finite/gdscphcet/ui/EventDetailActivity.kt
@@ -2,6 +2,8 @@ package com.finite.gdscphcet.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.fragment.app.viewModels
@@ -12,6 +14,9 @@ import com.finite.gdscphcet.databinding.ActivityMainBinding
 import com.finite.gdscphcet.databinding.FragmentHomeBinding
 import com.finite.gdscphcet.ui.viewModel.AboutViewModel
 import com.finite.gdscphcet.ui.viewModel.EventDetailViewModel
+import com.finite.gdscphcet.ui.viewModel.PastEvents
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class EventDetailActivity : AppCompatActivity() {
 
@@ -29,9 +34,50 @@ class EventDetailActivity : AppCompatActivity() {
             eventViewModel = viewModel
         }
 
+        val arguments = args.eventid
+        setEventData(arguments)
+    }
 
-        val toasttest = args.eventid
-        Toast.makeText(this,toasttest,Toast.LENGTH_SHORT).show()
+
+
+    private lateinit var database : DatabaseReference
+
+    fun setEventData(eventId: String) {
+
+        database = FirebaseDatabase.getInstance().getReference("pastEvents")
+        database.child(eventId).get().addOnSuccessListener {
+            if (it.exists()){
+
+                Log.d("success", "Hua")
+                val title = it.child("title").value.toString()
+                val date = it.child("date").value.toString()
+                val time = it.child("time").value.toString()
+                val mode = it.child("mode").value.toString()
+                val shortdesc = it.child("shortdesc").value.toString()
+                val eventlink = it.child("eventlink").value.toString()
+                val posterlink = it.child("posterlink").value.toString()
+                val videolink = it.child("videolink").value.toString()
+
+                Toast.makeText(this, "$title : $date",Toast.LENGTH_SHORT).show()
+
+
+
+
+
+
+
+
+
+
+            }else{
+                Toast.makeText(this,"does not exists",Toast.LENGTH_SHORT).show()
+                Log.d("DoesNotExists", " nai hai data bhai :(")
+            }
+
+        }.addOnFailureListener{
+            Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+            Log.d("Failed","fail zhaala")
+        }
     }
 
     fun callFinish() {
