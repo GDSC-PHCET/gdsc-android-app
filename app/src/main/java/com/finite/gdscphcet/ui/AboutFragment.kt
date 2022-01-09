dbbac52f1e69f98dfd59544115190b688a7ea027
Fin1te <rishabhmehta00@gmail.com>
2022-01-09 17:56:22 +0530

    Implemented AboutFragment

--- app/src/main/java/com/finite/gdscphcet/ui/AboutFragment.kt
+++ app/src/main/java/com/finite/gdscphcet/ui/AboutFragment.kt
@@ -1,15 +1,21 @@
 package com.finite.gdscphcet.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
 import com.finite.gdscphcet.databinding.FragmentAboutBinding
+import com.finite.gdscphcet.ui.viewModel.AboutViewModel
 
 class AboutFragment : Fragment() {
 
     private var binding : FragmentAboutBinding? = null
+    private val viewModel: AboutViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,4 +26,20 @@ class AboutFragment : Fragment() {
         binding = fragmentBinding
         return fragmentBinding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            aboutFragment = this@AboutFragment
+            aboutViewModel = viewModel
+        }
+    }
+
+    fun openLink(link: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+        startActivity(intent)
+    }
+
+
 }
