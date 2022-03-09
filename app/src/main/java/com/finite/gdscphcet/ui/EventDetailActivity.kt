89d5b4f80ff88b56cc2342ab1c476669c1881c29
Mohit Gupta <guptamm.22@gmail.com>
2022-03-09 23:54:54 +0530

    upcoming events layout

--- app/src/main/java/com/finite/gdscphcet/ui/EventDetailActivity.kt
+++ app/src/main/java/com/finite/gdscphcet/ui/EventDetailActivity.kt
@@ -10,17 +10,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.navArgs
-import androidx.navigation.navArgument
 import com.bumptech.glide.Glide
 import com.finite.gdscphcet.R
 import com.finite.gdscphcet.databinding.ActivityEventDetailBinding
-import com.finite.gdscphcet.databinding.ActivityMainBinding
-import com.finite.gdscphcet.databinding.FragmentHomeBinding
-import com.finite.gdscphcet.ui.viewModel.AboutViewModel
 import com.finite.gdscphcet.ui.viewModel.EventDetailViewModel
-import com.finite.gdscphcet.ui.viewModel.PastEvents
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -28,6 +22,14 @@ class EventDetailActivity : AppCompatActivity() {
 
     val args: EventDetailActivityArgs by navArgs()
     private val viewModel: EventDetailViewModel by viewModels()
+    private lateinit var videoButton: Button
+    private lateinit var posterImv : ImageView
+    private lateinit var tvEventTitle : TextView
+    private lateinit var tvEventDate : TextView
+    private lateinit var tvEventTime : TextView
+    private lateinit var tvEventMode : TextView
+    private lateinit var tvAboutDetails : TextView
+    private lateinit var eventButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,19 +42,32 @@ class EventDetailActivity : AppCompatActivity() {
             eventViewModel = viewModel
         }
 
-        val pastEventId = intent.getStringExtra("eventId")
+        videoButton = binding.videoButton
+        posterImv = binding.PosterImv
+        tvEventTitle = binding.tvEventTitle
+        tvEventDate = binding.tvEventDate
+        tvEventTime = binding.tvEventTime
+        tvEventMode = binding.tvEventMode
+        tvAboutDetails = binding.tvAboutDetails
+        eventButton = binding.eventButton
 
-        Log.d("PRI", "success $pastEventId")
-        if (pastEventId != null) {
-            setEventData(pastEventId)
+        val eventId = intent.getStringExtra("eventId")
+        val eventTicketLink = intent.getStringExtra("eventTicket")
+
+        Log.d("PRI", "success $eventId")
+        if (eventId != null) {
+            if (eventTicketLink != null) {
+                setUpcomingEventData(eventId)
+                videoButton.setText(getString(R.string.bookEventTicket))
+            }
+            else
+                setPastEventData(eventId)
         }
     }
 
-
-
     private lateinit var database : DatabaseReference
 
-    private fun setEventData(eventId: String) {
+    private fun setPastEventData(eventId: String) {
 
         val posterImv : ImageView = findViewById(R.id.PosterImv)
         val tvEventTitle : TextView = findViewById(R.id.tvEventTitle)
@@ -60,7 +75,6 @@ class EventDetailActivity : AppCompatActivity() {
         val tvEventTime : TextView = findViewById(R.id.tvEventTime)
         val tvEventMode : TextView = findViewById(R.id.tvEventMode)
         val tvAboutDetails : TextView = findViewById(R.id.tvAboutDetails)
-        val videoButton: Button = findViewById(R.id.videoButton)
         val eventButton: Button = findViewById(R.id.eventButton)
 
         database = FirebaseDatabase.getInstance().getReference("pastEvents")
@@ -97,6 +111,53 @@ class EventDetailActivity : AppCompatActivity() {
                     startActivity(intent)
                 }
 
+
+            }else{
+                Toast.makeText(this,"does not exists",Toast.LENGTH_SHORT).show()
+                Log.d("DoesNotExists", " nai hai data bhai :(")
+            }
+
+        }.addOnFailureListener{
+            Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+            Log.d("Failed","fail zhaala")
+        }
+    }
+
+    private fun setUpcomingEventData(eventId: String) {
+
+        database = FirebaseDatabase.getInstance().getReference("upcomingEvents")
+        database.child(eventId).get().addOnSuccessListener {
+            if (it.exists()){
+
+                Log.d("success", "Hua")
+                val title = it.child("title").value.toString()
+                val date = it.child("date").value.toString()
+                val time = it.child("time").value.toString()
+                val mode = it.child("mode").value.toString()
+                val shortdesc = it.child("shortdesc").value.toString()
+                val eventlink = it.child("eventlink").value.toString()
+                val posterlink = it.child("posterlink").value.toString()
+                val videolink = it.child("videolink").value.toString()
+
+                //Toast.makeText(this, "$title : $date",Toast.LENGTH_SHORT).show()
+
+                Glide.with(this).load(posterlink).error(R.drawable.ic_404_image).centerCrop().into(posterImv)
+
+                tvEventTitle.text = title
+                tvEventDate.text = "📅 \t\t\t:\t\t$date"
+                tvEventTime.text = "⌚ \t\t\t:\t\t$time"
+                tvEventMode.text = "📍 \t\t\t:\t\t$mode"
+                tvAboutDetails.text = shortdesc
+
+                videoButton.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(videolink))
+                    startActivity(intent)
+                }
+
+                eventButton.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(eventlink))
+                    startActivity(intent)
+                }
 
 
             }else{
--- app/src/main/java/com/finite/gdscphcet/ui/HomeFragment.kt
+++ app/src/main/java/com/finite/gdscphcet/ui/HomeFragment.kt
@@ -11,18 +11,22 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.finite.gdscphcet.R
 import com.finite.gdscphcet.adapters.PastEventsAdapter
+import com.finite.gdscphcet.adapters.UpcomingEventsAdapter
 import com.finite.gdscphcet.databinding.FragmentHomeBinding
 import com.finite.gdscphcet.model.PastEventModel
+import com.finite.gdscphcet.model.UpcomingEventModel
 import com.finite.gdscphcet.ui.viewModel.HomeViewModel
 import com.google.firebase.database.*
 
 class HomeFragment : Fragment() {
 
-    private lateinit var dbref : DatabaseReference
-    private var binding : FragmentHomeBinding? = null
+    private lateinit var dbref: DatabaseReference
+    private var binding: FragmentHomeBinding? = null
     private val viewModel: HomeViewModel by viewModels()
     private lateinit var pastEventsRecyclerView: RecyclerView
     private lateinit var pastEventsList: ArrayList<PastEventModel>
+    private lateinit var upcomingEventsRecyclerView: RecyclerView
+    private lateinit var upcomingEventsList: ArrayList<UpcomingEventModel>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,11 +51,18 @@ class HomeFragment : Fragment() {
         }
 
         pastEventsRecyclerView = binding!!.rvPastEvents
-        pastEventsRecyclerView.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+        pastEventsRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         pastEventsRecyclerView.setHasFixedSize(true)
         pastEventsList = arrayListOf()
         getPastEventsData()
 
+        upcomingEventsRecyclerView = binding!!.rvUpcomingEvents
+        upcomingEventsRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        upcomingEventsRecyclerView.setHasFixedSize(true)
+        upcomingEventsList = arrayListOf()
+        getUpcomingEventsData()
     }
 
     private fun getPastEventsData() {
@@ -61,15 +72,50 @@ class HomeFragment : Fragment() {
 
             override fun onDataChange(snapshot: DataSnapshot) {
 
-                if (snapshot.exists()){
+                if (snapshot.exists()) {
 
-                    for (eventSnapshot in snapshot.children){
+                    for (eventSnapshot in snapshot.children) {
 
                         val event = eventSnapshot.getValue(PastEventModel::class.java)
                         pastEventsList.add(event!!)
 
                     }
-                    pastEventsRecyclerView.adapter = PastEventsAdapter(requireContext(),pastEventsList.reversed() as MutableList<PastEventModel>)
+                    pastEventsRecyclerView.adapter = PastEventsAdapter(
+                        requireContext(),
+                        pastEventsList.reversed() as MutableList<PastEventModel>
+                    )
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println(error)
+            }
+
+
+        })
+    }
+
+    private fun getUpcomingEventsData() {
+        dbref = FirebaseDatabase.getInstance().getReference("upcomingEvents")
+
+        dbref.addListenerForSingleValueEvent(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()) {
+                    upcomingEventsRecyclerView.visibility = View.VISIBLE
+                    binding?.upcomingEventsPlaceholderLayout!!.visibility = View.GONE
+
+                    for (eventSnapshot in snapshot.children) {
+                        val event = eventSnapshot.getValue(UpcomingEventModel::class.java)
+                        upcomingEventsList.add(event!!)
+                    }
+                    upcomingEventsRecyclerView.adapter = UpcomingEventsAdapter(
+                        requireContext(),
+                        upcomingEventsList.reversed() as MutableList<UpcomingEventModel>
+                    )
+                } else{
+                    upcomingEventsRecyclerView.visibility = View.GONE
                 }
             }
 
