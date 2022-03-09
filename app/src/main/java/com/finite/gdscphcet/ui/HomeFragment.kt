18d6be9cd9d6b57908fbab3bf445c5994b2f78eb
priyanshuborole <ppborole1762002@gmail.com>
2022-03-09 20:00:58 +0530

    modified past events

--- app/src/main/java/com/finite/gdscphcet/ui/HomeFragment.kt
+++ app/src/main/java/com/finite/gdscphcet/ui/HomeFragment.kt
@@ -7,20 +7,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.finite.gdscphcet.R
+import com.finite.gdscphcet.adapters.PastEventsAdapter
 import com.finite.gdscphcet.databinding.FragmentHomeBinding
+import com.finite.gdscphcet.model.PastEventModel
 import com.finite.gdscphcet.ui.viewModel.HomeViewModel
+import com.google.firebase.database.*
 
 class HomeFragment : Fragment() {
 
+    private lateinit var dbref : DatabaseReference
     private var binding : FragmentHomeBinding? = null
     private val viewModel: HomeViewModel by viewModels()
+    private lateinit var pastEventsRecyclerView: RecyclerView
+    private lateinit var pastEventsList: ArrayList<PastEventModel>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         val fragmentBinding = FragmentHomeBinding.inflate(inflater, container, false)
         binding = fragmentBinding
         return fragmentBinding.root
@@ -39,10 +46,40 @@ class HomeFragment : Fragment() {
             findNavController().navigate(action)
         }
 
+        pastEventsRecyclerView = binding!!.rvPastEvents
+        pastEventsRecyclerView.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+        pastEventsRecyclerView.setHasFixedSize(true)
+        pastEventsList = arrayListOf()
+        getPastEventsData()
+
     }
 
-    fun openDetailActivity(id : String) {
-        val action = HomeFragmentDirections.actionHomeFragmentToEventDetailActivity(id)
-        findNavController().navigate(action)
+    private fun getPastEventsData() {
+        dbref = FirebaseDatabase.getInstance().getReference("pastEvents")
+
+        dbref.addListenerForSingleValueEvent(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()){
+
+                    for (eventSnapshot in snapshot.children){
+
+                        val event = eventSnapshot.getValue(PastEventModel::class.java)
+                        pastEventsList.add(event!!)
+
+                    }
+                    pastEventsRecyclerView.adapter = PastEventsAdapter(requireContext(),pastEventsList.reversed() as MutableList<PastEventModel>)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println(error)
+            }
+
+
+        })
     }
+
+
 }
