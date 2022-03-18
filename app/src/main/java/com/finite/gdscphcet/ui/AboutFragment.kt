b990a7521cfc37d554c31d7d161744c9746f56dc
Mohit Gupta <guptamm.22@gmail.com>
2022-03-19 01:38:50 +0530

    remove unwanted import

--- app/src/main/java/com/finite/gdscphcet/ui/AboutFragment.kt
+++ app/src/main/java/com/finite/gdscphcet/ui/AboutFragment.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.finite.gdscphcet.databinding.FragmentAboutBinding
