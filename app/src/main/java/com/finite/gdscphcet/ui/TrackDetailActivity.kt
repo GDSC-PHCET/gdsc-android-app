2006b5d9ceb336d2331e51ff0264b6895808ad7a
Souvik Dey <deysouvik700@gmail.com>
2022-04-09 01:14:19 +0530

    issue#69 solved

--- app/src/main/java/com/finite/gdscphcet/ui/TrackDetailActivity.kt
+++ app/src/main/java/com/finite/gdscphcet/ui/TrackDetailActivity.kt
@@ -2,6 +2,7 @@ package com.finite.gdscphcet.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.navArgs
 import com.finite.gdscphcet.R
@@ -9,9 +10,16 @@ import com.finite.gdscphcet.R
 class TrackDetailActivity : AppCompatActivity() {
     private val args by navArgs<TrackDetailActivityArgs>()
 
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_track_detail)
+
+        toolbar=findViewById(R.id.toolbar_trackDetailActivity)
+        setSupportActionBar(toolbar)
+        toolbar.title="GDSC PHCET"
+
 
         val position = args.position
     }
