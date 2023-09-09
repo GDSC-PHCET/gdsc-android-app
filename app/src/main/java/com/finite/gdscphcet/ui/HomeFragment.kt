21ce3270d2c22a5d6639d697d527b32eaa0fa3bd
Rishabh Mehta <rishabhmehta00@gmail.com>
2023-09-09 20:10:57 +0530

    Migrated Data Fetching logic (via Repo) from HomeFragment to ViewModel

--- app/src/main/java/com/finite/gdscphcet/ui/HomeFragment.kt
+++ app/src/main/java/com/finite/gdscphcet/ui/HomeFragment.kt
@@ -4,25 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.facebook.shimmer.ShimmerFrameLayout
-import com.finite.gdscphcet.adapters.PastEventsAdapter
-import com.finite.gdscphcet.adapters.UpcomingEventsAdapter
 import com.finite.gdscphcet.databinding.FragmentHomeBinding
-import com.finite.gdscphcet.repository.PastEventRepo
-import com.finite.gdscphcet.repository.UpcomingEventRepo
-import com.finite.scrapingpractise.model.PastEvent
-import com.finite.scrapingpractise.model.UpcomingEvent
 import com.nabilmh.lottieswiperefreshlayout.LottieSwipeRefreshLayout
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class HomeFragment : Fragment() {
 
@@ -30,17 +17,9 @@ class HomeFragment : Fragment() {
 
     private lateinit var pastEventsRecyclerView: RecyclerView
     private lateinit var upcomingEventsRecyclerView: RecyclerView
-
-    private lateinit var upcomingPlaceHolder: LinearLayout
-    private lateinit var upcomingShimmer: ShimmerFrameLayout
-    private lateinit var pastShimmer: ShimmerFrameLayout
-
     private lateinit var swipeRefreshLayout: LottieSwipeRefreshLayout
+    private val homeViewModel: HomeViewModel by viewModels()
 
-//        private val url = "https://gdsc.community.dev/pillai-hoc-college-of-engineering-and-technology-navi-mumbai/"
-//    private val url = "https://gdsc.community.dev/dy-patil-college-of-engineering-pune/"
-//        private val url = "https://gdsc.community.dev/mody-university-of-science-and-technology-laxmangarh/"
-     private val url = "https://gdsc.community.dev/sir-syed-university-of-engineering-and-technology/"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,33 +33,18 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initialiseShimmer()
         initialiseRecyclerViews()
-        updateEvents()
 
+        if (homeViewModel.pastEvents.value!!.isNotEmpty()) {
+            homeViewModel.loadPastEvents(binding!!)
+            homeViewModel.loadUpcomingEvents(binding!!)
+        } else {
+            updateData()
+        }
 
         swipeRefreshLayout = binding!!.homeSwipeRefreshLayout
         swipeRefreshLayout.setOnRefreshListener {
-            updateEvents()
-        }
-    }
-
-    private fun updateEvents() {
-        upcomingEventsRecyclerView.visibility = View.GONE
-        pastEventsRecyclerView.visibility = View.GONE
-        upcomingPlaceHolder.visibility = View.GONE
-
-        upcomingShimmer.visibility = View.VISIBLE
-        pastShimmer.visibility = View.VISIBLE
-        upcomingShimmer.startShimmer()
-        pastShimmer.startShimmer()
-
-        CoroutineScope(Dispatchers.Main).launch {
-            getPastEvents()
-        }
-
-        CoroutineScope(Dispatchers.Main).launch {
-            getUpcomingEvents()
+            updateData()
         }
     }
 
@@ -98,57 +62,8 @@ class HomeFragment : Fragment() {
         upcomingEventsRecyclerView.setHasFixedSize(true)
     }
 
-    private fun initialiseShimmer() {
-        upcomingShimmer = binding!!.shimmerUpcomingEvent
-        pastShimmer = binding!!.shimmerPastEvent
-        upcomingPlaceHolder = binding!!.upcomingEventsPlaceholderLayout
-
-        upcomingShimmer.startShimmer()
-        pastShimmer.startShimmer()
-    }
-
-    private suspend fun getPastEvents() {
-
-        var pastEvents = mutableListOf<PastEvent>()
-
-        withContext(Dispatchers.IO) {
-            val job = async {
-                PastEventRepo.getPastEventsList(url)
-            }
-            pastEvents = job.await()
-        }
-
-        withContext(Dispatchers.Main) {
-            pastEventsRecyclerView.adapter = PastEventsAdapter(pastEvents)
-            pastShimmer.stopShimmer()
-            pastShimmer.visibility = View.GONE
-            pastEventsRecyclerView.visibility = View.VISIBLE
-            if (swipeRefreshLayout.isRefreshing) swipeRefreshLayout.isRefreshing = false
-        }
-    }
-
-    private suspend fun getUpcomingEvents() {
-
-        var upcomingEvents = mutableListOf<UpcomingEvent>()
-
-        withContext(Dispatchers.IO) {
-            val job = async {
-                UpcomingEventRepo.getUpcomingEventsList(url)
-            }
-            upcomingEvents = job.await()
-        }
-
-        withContext(Dispatchers.Main) {
-            upcomingShimmer.stopShimmer()
-            upcomingShimmer.visibility = View.GONE
-            if (swipeRefreshLayout.isRefreshing) swipeRefreshLayout.isRefreshing = false
-            if (upcomingEvents.isEmpty()) {
-                upcomingEventsRecyclerView.visibility = View.GONE
-                upcomingPlaceHolder.visibility = View.VISIBLE
-            } else {
-                upcomingEventsRecyclerView.adapter = UpcomingEventsAdapter(upcomingEvents)
-                upcomingEventsRecyclerView.visibility = View.VISIBLE
-            }
-        }
+    private fun updateData() {
+        homeViewModel.loadPastEvents(binding!!, true)
+        homeViewModel.loadUpcomingEvents(binding!!, true)
     }
 }
---
+++ app/src/main/java/com/finite/gdscphcet/ui/HomeViewModel.kt
@@ -0,0 +1,132 @@
+package com.finite.gdscphcet.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.finite.gdscphcet.adapters.PastEventsAdapter
+import com.finite.gdscphcet.adapters.UpcomingEventsAdapter
+import com.finite.gdscphcet.databinding.FragmentHomeBinding
+import com.finite.gdscphcet.repository.PastEventRepo
+import com.finite.gdscphcet.repository.UpcomingEventRepo
+import com.finite.scrapingpractise.model.PastEvent
+import com.finite.scrapingpractise.model.UpcomingEvent
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class HomeViewModel : ViewModel() {
+
+    // TODO : Fix App crash on internet connection off
+
+    private val _pastEvents = MutableLiveData<List<PastEvent>?>(listOf())
+    val pastEvents: LiveData<List<PastEvent>?> = _pastEvents
+
+    private val _upcomingEvents = MutableLiveData<List<UpcomingEvent>?>()
+    val upcomingEvents: LiveData<List<UpcomingEvent>?> = _upcomingEvents
+
+    private val url =
+        "https://gdsc.community.dev/pillai-hoc-college-of-engineering-and-technology-navi-mumbai/"
+//    private val url = "https://gdsc.community.dev/dy-patil-college-of-engineering-pune/"
+//    private val url = "https://gdsc.community.dev/mody-university-of-science-and-technology-laxmangarh/"
+//    private val url = "https://gdsc.community.dev/sir-syed-university-of-engineering-and-technology/"
+
+
+    private fun clearEvents() {
+        _pastEvents.value = null
+        _upcomingEvents.value = null
+    }
+
+    // observe the past events and update the UI
+    private fun observePastEvents(binding: FragmentHomeBinding) {
+        pastEvents.observeForever {
+
+            if (it != null) {
+                viewModelScope.launch {
+                    binding.rvPastEvents.adapter = PastEventsAdapter(_pastEvents.value!!)
+                    binding.shimmerPastEvent.visibility = android.view.View.GONE
+                    binding.shimmerPastEvent.stopShimmer()
+                    binding.rvPastEvents.visibility = android.view.View.VISIBLE
+                    if (binding.homeSwipeRefreshLayout.isRefreshing) binding.homeSwipeRefreshLayout.isRefreshing =
+                        false
+                }
+            }
+        }
+    }
+
+    private fun observeUpcomingEvents(binding: FragmentHomeBinding) {
+        upcomingEvents.observeForever {
+            if (it != null) {
+                if (_upcomingEvents.value!!.isEmpty()) {
+                    binding.upcomingEventsPlaceholderLayout.visibility = android.view.View.VISIBLE
+                    binding.shimmerUpcomingEvent.visibility = android.view.View.GONE
+                    binding.shimmerUpcomingEvent.stopShimmer()
+                    binding.rvUpcomingEvents.visibility = android.view.View.GONE
+                } else {
+                    viewModelScope.launch {
+                        binding.rvUpcomingEvents.adapter =
+                            UpcomingEventsAdapter(_upcomingEvents.value!!)
+                        binding.shimmerUpcomingEvent.visibility = android.view.View.GONE
+                        binding.upcomingEventsPlaceholderLayout.visibility = android.view.View.GONE
+                        binding.shimmerUpcomingEvent.stopShimmer()
+                        binding.rvUpcomingEvents.visibility = android.view.View.VISIBLE
+                    }
+                }
+
+                if (binding.homeSwipeRefreshLayout.isRefreshing) binding.homeSwipeRefreshLayout.isRefreshing =
+                    false
+            }
+        }
+    }
+
+    fun loadPastEvents(binding: FragmentHomeBinding, update: Boolean = false) {
+
+        binding.shimmerPastEvent.startShimmer()
+        binding.shimmerPastEvent.visibility = android.view.View.VISIBLE
+        binding.rvPastEvents.visibility = android.view.View.GONE
+
+        if (update) {
+            viewModelScope.launch {
+                clearEvents()
+                val job = async(Dispatchers.IO) {
+                    PastEventRepo.getPastEventsList(url)
+                }
+                val pastEvents = job.await()
+                _pastEvents.postValue(pastEvents)
+                observePastEvents(binding)
+            }
+        } else {
+            viewModelScope.launch {
+                delay(500)
+                observePastEvents(binding)
+            }
+
+        }
+    }
+
+    fun loadUpcomingEvents(binding: FragmentHomeBinding, update: Boolean = false) {
+
+        binding.shimmerUpcomingEvent.startShimmer()
+        binding.shimmerUpcomingEvent.visibility = android.view.View.VISIBLE
+        binding.rvUpcomingEvents.visibility = android.view.View.GONE
+        binding.upcomingEventsPlaceholderLayout.visibility = android.view.View.GONE
+
+        if (update) {
+            viewModelScope.launch {
+                clearEvents()
+                val job = async(Dispatchers.IO) {
+                    UpcomingEventRepo.getUpcomingEventsList(url)
+                }
+                val upcomingEvents = job.await()
+                _upcomingEvents.postValue(upcomingEvents)
+                observeUpcomingEvents(binding)
+            }
+        } else {
+            viewModelScope.launch {
+                delay(500)
+                observeUpcomingEvents(binding)
+            }
+        }
+    }
+}
